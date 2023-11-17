da42d4facfebcb8476fa5e0ec4c06fd0c2cd5a1f
Goto-star <goyu620@gmail.com>
2023-11-17 14:08:30 +0900

    指摘された箇所の修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -7,6 +7,26 @@ require 'optparse'
 
 SPLIT_NUMBER = 3
 
+FILE_TYPE = {
+  '01' => 'p',
+  '02' => 'c',
+  '04' => 'd',
+  '06' => 'b',
+  '10' => '-',
+  '12' => 'l',
+  '14' => 's'
+}.freeze
+
+FILE_MODE = {
+  '0' => '---',
+  '1' => '--x',
+  '3' => '-w-',
+  '4' => 'r--',
+  '5' => 'r-x',
+  '6' => 'rw-',
+  '7' => 'rwx'
+}.freeze
+
 def main
   options = ARGV.getopts('l')
   files = Dir.glob('*')
@@ -14,7 +34,7 @@ def main
 end
 
 def display_long_format(files)
-  long_formats = fetch_long_format(files)
+  long_formats = gain_long_format(files)
   total_block_number = calc_total_block_number(long_formats)
   max_width = calc_max_width(long_formats)
 
@@ -22,42 +42,33 @@ def display_long_format(files)
   long_formats.each do |format|
     print "#{format[:file_mode]}  "
     print "#{format[:hard_links_count].to_s.rjust(max_width[:link])} "
-    print "#{format[:owner].rjust(max_width[:owner])}  "
-    print "#{format[:group].rjust(max_width[:group])}  "
+    print "#{format[:owner].ljust(max_width[:owner])}  "
+    print "#{format[:group].ljust(max_width[:group])}  "
     print "#{format[:file_size].to_s.rjust(max_width[:file_size])} "
     print "#{format[:day_time]} "
     print "#{format[:file_name]}\n"
   end
 end
 
-def fetch_long_format(files)
-  long_formats = []
-
-  files.each do |file|
+def gain_long_format(files)
+  files.map do |file|
     file_info = File.stat(file)
-    long_format = {
+    {
       block: file_info.blocks,
       file_mode: fetch_file_mode(file_info),
       hard_links_count: file_info.nlink,
       owner: Etc.getpwuid(file_info.uid).name,
       group: Etc.getgrgid(file_info.gid).name,
       file_size: file_info.size,
-      day_time: fetch_last_modified_day(file_info),
+      day_time: file_info.mtime.strftime('%_m %_d %H:%M'),
       file_name: File.basename(file)
     }
-    long_formats << long_format
   end
-  long_formats
 end
 
 def calc_total_block_number(long_formats)
-  blocks = []
-
-  long_formats.each do |long_format|
-    file_block = long_format[:block]
-    blocks << file_block
-  end
-  blocks.sum
+  block_long_formats = long_formats.map { |long_format| long_format.fetch(:block) }
+  block_long_formats.sum
 end
 
 def calc_max_width(long_formats)
@@ -72,42 +83,15 @@ end
 def fetch_file_mode(file_info)
   file_mode_of_numeric = file_info.mode.to_s(8)
   file_mode_symbolic = convert_file_mode(file_mode_of_numeric.slice(3, 3))
-  file_type_of_symboric = convert_file_type(file_mode_of_numeric.slice(0, 2))
+  file_type_of_symboric = FILE_TYPE[file_mode_of_numeric.slice(0, 2)]
   "#{file_type_of_symboric}#{file_mode_symbolic}"
 end
 
-def convert_file_type(file_type_number)
-  {
-    '01' => 'p',
-    '02' => 'c',
-    '04' => 'd',
-    '06' => 'b',
-    '10' => '-',
-    '12' => 'l',
-    '14' => 's'
-  }[file_type_number]
-end
-
 def convert_file_mode(file_mode_numbers)
-  file_mode_symbolic = []
-  file_mode_numbers.chars.each do |number|
-    file_mode = {
-      '0' => '---',
-      '1' => '--x',
-      '3' => '-w-',
-      '4' => 'r--',
-      '5' => 'r-x',
-      '6' => 'rw-',
-      '7' => 'rwx'
-    }[number]
-    file_mode_symbolic << file_mode
+  file_mode_numbers.chars.map do |number|
+    file_mode_numbers[number] = FILE_MODE[number]
   end
-  file_mode_symbolic.join
-end
-
-def fetch_last_modified_day(file_info)
-  last_modified_day = file_info.mtime
-  last_modified_day.strftime('%_m %_d %H:%M')
+  file_mode_numbers
 end
 
 def display_files(files)
